5ceb57da4dcec46639dbe7a20ec59c852faa4c44
Darius <30323313+D4R1U507364@users.noreply.github.com>
2017-08-01 12:23:33 -0400

    Add files via upload

---
+++ List.py
@@ -0,0 +1,9 @@
+import time
+print('This is a list of numbers.');
+time.sleep(2)
+print('It goes on for a while.');
+time.sleep(2)
+print('You have been warned.');
+time.sleep(2)
+for i in range(1000000000000000000):
+    print(i)
